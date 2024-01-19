27c25a098cd1b95582765b51f2f616f1389ed000
Aviral Jain <aviraljainltr@gmail.com>
2024-01-20 01:00:15 +0530

    Created MongoDB schemas and models
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const express = require('express');
-require('./config/dbConnect')
+const express = require("express");
+require("./config/dbConnect");
 
 const app = express();
 
 //server
-const PORT = process.env.PORT || 9000
-app.listen(PORT, console.log("Server is running on port "+ PORT))
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, console.log("Server is running on port " + PORT));
