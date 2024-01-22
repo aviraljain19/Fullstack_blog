098f4e9932078755b7b464b043f0316723973b83
Aviral Jain <aviraljainltr@gmail.com>
2024-01-22 12:13:05 +0530

    Configured server to serve static files
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 
 app.use(express.json());
 
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname,+'/public'));
+
 app.use(
   session({
     secret: process.env.SESSION_KEY,
